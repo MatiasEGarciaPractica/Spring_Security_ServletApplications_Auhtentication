a14bf871d2c54709783ae2acabffddfc5df7e7a5
Matias.E.Garcia <matiasegarcia22@gmail.com>
2024-04-16 08:27:35 -0300

    client cannot create a second session when first is active

--- src/main/java/com/auth/config/SecurityConfig.java
+++ src/main/java/com/auth/config/SecurityConfig.java
@@ -12,7 +12,11 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.COOKIES;
 
 @Configuration
 @EnableWebSecurity
@@ -23,9 +27,20 @@ public class SecurityConfig {
         http.formLogin(Customizer.withDefaults());
         http.sessionManagement(session ->
                 session.maximumSessions(1)//will prevent a user from logging in multiple times.
+                        .maxSessionsPreventsLogin(true) //with this if client tries to login 2 times, second time will not be able to.
                 );
+
+        //http.sessionManagement(session -> session.invalidSessionUrl("")); //with this when session finish but client make a request,we can send to the url
+
+        //you should clear out the cookie of session when a session is expired or client logout
+        http.logout(logout ->
+                logout.addLogoutHandler(new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(COOKIES)))); //delete the cookie
+        //an alternative is the next line
+        http.logout(l -> l.deleteCookies("JSESSIONID"));//better to delete some specific cookie
+
         return http.build();
     }
+
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
--- src/test/java/com/auth/SessionTest.java
+++ src/test/java/com/auth/SessionTest.java
@@ -20,24 +20,38 @@ public class SessionTest {
     @Autowired
     private MockMvc mvc;
 
-    @Test//this test works if you quit this line from session management : .maxSessionsPreventsLogin(true)
-    void loginOnSecondLoginThenFirstSessionTerminated() throws Exception{
-        //perfom a login and we expect to be authenticated.
-    MvcResult mvcResult = this.mvc.perform(formLogin())
-            .andExpect(authenticated()).andReturn();
+//    @Test//this test works if you quit this line from session management : .maxSessionsPreventsLogin(true)
+//    void loginOnSecondLoginThenFirstSessionTerminated() throws Exception{
+//        //perfom a login and we expect to be authenticated.
+//    MvcResult mvcResult = this.mvc.perform(formLogin())
+//            .andExpect(authenticated()).andReturn();
+//
+//    //retrieves the session from the first login
+//    MockHttpSession firstLoginSession = (MockHttpSession) mvcResult.getRequest().getSession();
+//
+//    //perfom a request and expect to be authenticated, still with first session.
+//    this.mvc.perform(get("/").session(firstLoginSession)).andExpect(authenticated());
+//
+//    //it performs another form login and expects the user to be authenticated.
+//    this.mvc.perform(formLogin()).andExpect(authenticated());
+//
+//    //it performs a request with the session from the first login and expects the user to be unauthenticated,
+//        // indicating that the first session has been terminated.
+//        this.mvc.perform(get("/").session(firstLoginSession)).andExpect(unauthenticated());
+//    }
 
-    //retrieves the session from the first login
-    MockHttpSession firstLoginSession = (MockHttpSession) mvcResult.getRequest().getSession();
+    @Test
+    void loginOnSecondLoginThenPreventLogin() throws Exception{
+        MvcResult mvcResult = this.mvc.perform(formLogin()).andExpect(authenticated()).andReturn();
 
-    //perfom a request and expect to be authenticated, still with first session.
-    this.mvc.perform(get("/").session(firstLoginSession)).andExpect(authenticated());
+        MockHttpSession firstLoginSession = (MockHttpSession) mvcResult.getRequest().getSession();
 
-    //it performs another form login and expects the user to be authenticated.
-    this.mvc.perform(formLogin()).andExpect(authenticated());
+        this.mvc.perform(get("/").session(firstLoginSession)).andExpect(authenticated());
 
-    //it performs a request with the session from the first login and expects the user to be unauthenticated,
-        // indicating that the first session has been terminated.
-        this.mvc.perform(get("/").session(firstLoginSession)).andExpect(unauthenticated());
+        // second login is prevented
+        this.mvc.perform(formLogin()).andExpect(unauthenticated());
 
+        // first session is still valid
+        this.mvc.perform(get("/").session(firstLoginSession)).andExpect(authenticated());
     }
 }
